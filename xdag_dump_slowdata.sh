b06c184ba102b21653c10310d1cbf1ae716ede0c
Viliam Kubis <viliam.kubis@gmail.com>
2018-04-21 19:27:29 +0200

    support for exporting found blocks by the pool

--- xdag_dump_slowdata.sh
+++ xdag_dump_slowdata.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 grep -Pha "`/usr/bin/php /home/pool/scripts/generate_last_days_regex.php`"'.+:MESS]  Xfer  : from [0-9a-zA-Z+/]{32}' /home/pool/xdag1/client/xdag.log /home/pool/xdag2/client/xdag.log | sort > /var/www/pool/payouts_tmp.txt
+grep -Pha "`/usr/bin/php /home/pool/scripts/generate_last_days_regex.php`"'.+:MESS]  Paid  : from [0-9a-f]{64}' /home/pool/xdag1/client/xdag.log /home/pool/xdag2/client/xdag.log | sort > /var/www/pool/blocks_tmp.txt
 
 sleep 1
 
 cp /var/www/pool/payouts_tmp.txt /var/www/pool/payouts.txt
+cp /var/www/pool/blocks_tmp.txt /var/www/pool/blocks.txt
