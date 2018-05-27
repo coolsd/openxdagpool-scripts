0a0d160ac39270d946c18574140e683a3889ad49
Ing. Viliam Kubis <viliam.kubis@gmail.com>
2018-05-28 01:55:20 +0200

    ignore number of spaces around Xfer and Paid command when parsing log files

--- xdag_dump_slowdata.sh
+++ xdag_dump_slowdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-grep -Pha "`/usr/bin/php /home/pool/scripts/generate_last_days_regex.php`"'.+:MESS]  Xfer  : from [0-9a-zA-Z+/]{32}' /home/pool/xdag1/client/xdag.log* /home/pool/xdag2/client/xdag.log* | sort > /var/www/pool/payouts_tmp.txt
-grep -Pha "`/usr/bin/php /home/pool/scripts/generate_last_days_regex.php`"'.+:INFO]  Paid  : [0-9a-f]{64}' /home/pool/xdag1/client/xdag.log* /home/pool/xdag2/client/xdag.log* | sort > /var/www/pool/blocks_tmp.txt
+grep -Pha "`/usr/bin/php /home/pool/scripts/generate_last_days_regex.php`"'.+:MESS]\s+Xfer\s*:\s*from [0-9a-zA-Z+/]{32}' /home/pool/xdag1/client/xdag.log* /home/pool/xdag2/client/xdag.log* | sort > /var/www/pool/payouts_tmp.txt
+grep -Pha "`/usr/bin/php /home/pool/scripts/generate_last_days_regex.php`"'.+:INFO]\s+Paid\s*:\s*[0-9a-f]{64}' /home/pool/xdag1/client/xdag.log* /home/pool/xdag2/client/xdag.log* | sort > /var/www/pool/blocks_tmp.txt
 
 sleep 1
 
