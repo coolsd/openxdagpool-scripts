1f5b7b5ce94b84281d34db4da9f47686954f1cbe
Viliam Kubis <viliam.kubis@gmail.com>
2018-06-03 00:29:38 +0200

    fixed DB schema

--- engine/db_schema.sql
+++ engine/db_schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE accounts(
 	last_inspected_at DATETIME,
 	hash VARCHAR(64) NULL UNIQUE,
 	payouts_sum DECIMAL(20, 9) NULL,
-	fee_percent_guessed DECIMAL(3, 3) NULL,
+	fee_percent_guessed DECIMAL(6, 3) NULL,
 	found_at DATETIME(3) NULL,
 	exported_at DATETIME(3) NULL,
 	invalidated_at DATETIME(3) NULL,
